2705b0d68545a441178975e60eac79c20cc56cbd
Sigmar Stefansson <sigmarkarl@gmail.com>
2013-10-07 01:31:18 +0000

    e

--- convertica/src/org/simmi/client/Compressor.java
+++ convertica/src/org/simmi/client/Compressor.java
@@ -5,6 +5,10 @@ import com.google.gwt.dom.client.DataTransfer;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.DoubleClickEvent;
+import com.google.gwt.event.dom.client.DoubleClickHandler;
 import com.google.gwt.event.dom.client.DragEndEvent;
 import com.google.gwt.event.dom.client.DragEndHandler;
 import com.google.gwt.event.dom.client.DragEnterEvent;
@@ -19,15 +23,15 @@ import com.google.gwt.event.dom.client.DragStartEvent;
 import com.google.gwt.event.dom.client.DragStartHandler;
 import com.google.gwt.event.dom.client.DropEvent;
 import com.google.gwt.event.dom.client.DropHandler;
-import com.google.gwt.event.logical.shared.ResizeEvent;
-import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.FileUpload;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+import elemental.client.Browser;
 import elemental.html.Blob;
 
 public class Compressor implements EntryPoint {
@@ -45,8 +49,17 @@ public class Compressor implements EntryPoint {
 		return dt.files.length;
 	}-*/;
 	
+	public native int numberOfFiles( Element e ) /*-{
+		$wnd.fc = 0;
+		return e.files.length;
+	}-*/;
+	
 	public native Blob readFile( DataTransfer dt, int i ) /*-{
 		return dt.files[ i ];
+	}-*/;
+	
+	public native Blob readFile( Element e, int i ) /*-{
+		return e.files[ i ];
 	}-*/;
 	
 	public native void setDownload( Element a, String dl ) /*-{
@@ -67,19 +80,38 @@ public class Compressor implements EntryPoint {
 	}-*/;
 	
 	public native void addFile( Blob blob, String fn, int nf ) /*-{		
-		var s = this;		
-		if( $wnd.zip == null ) $wnd.zip = new $wnd.Zlib.Zip();
-		
+		var s = this;
 		var fileReader = new FileReader();
-		fileReader.onload = function() {
-		    $wnd.zip.addFile( new Uint8Array(this.result), {
-		    	filename: $wnd.stringToByteArray( fn )
-			});
+		if( nf == 1 && fn.indexOf(".zip") != -1 ) {
+			fileReader.onload = function() {
+				$wnd.zip = new $wnd.Zlib.Unzip( new Uint8Array(this.result) );
+			    var filenames = $wnd.zip.getFilenames();
+			    for( var fkey in filenames ) {
+			    	var filename = filenames[fkey];
+					var array = $wnd.zip.decompress( filename );
+					var blob = new Blob( [array], {"type" : "application\/unknown" } );
+					s.@org.simmi.client.Compressor::downloadunzipped(Ljava/lang/String;Lelemental/html/Blob;)( filename, blob );
+			    }
+			    $wnd.zip = null;
+			};
+			fileReader.readAsArrayBuffer(blob);
+		} else {
+			if( $wnd.zip == null ) $wnd.zip = new $wnd.Zlib.Zip();
 			
-			$wnd.fc++;
-			if( $wnd.fc == nf ) s.@org.simmi.client.Compressor::download(Ljava/lang/String;)( fn );
-		};
-		fileReader.readAsArrayBuffer(blob);
+			fileReader.onload = function() {
+			    $wnd.zip.addFile( new Uint8Array(this.result), {
+			    	filename: $wnd.stringToByteArray( fn )
+				});
+				
+				$wnd.fc++;
+				if( $wnd.fc == nf ) s.@org.simmi.client.Compressor::download(Ljava/lang/String;)( fn );
+			};
+			fileReader.readAsArrayBuffer(blob);
+		}
+	}-*/;
+	
+	public native void click( Element e ) /*-{
+		e.click();
 	}-*/;
 	
 	public native Blob compress() /*-{
@@ -102,6 +134,17 @@ public class Compressor implements EntryPoint {
 		}
 	}-*/;
 	
+	public void downloadunzipped( String filename, Blob blob ) {
+		String		url = createObjectUrl( blob );
+		
+		String newname = filename;
+		Anchor anchor = new Anchor( newname );
+		anchor.setHref( url );
+		//subvp.add( anchor );
+		setDownload( anchor.getElement(), newname );
+		//Window.open( url, firstname+".zip", "_blank" );
+	}
+	
 	public void download( String filename ) {
 		int i = filename.lastIndexOf('.');
 		if( i != -1 ) filename = filename.substring(0,i);
@@ -121,14 +164,20 @@ public class Compressor implements EntryPoint {
 	public void onModuleLoad() {
 		init();
 		
-		final RootPanel	rp = RootPanel.get();
-		Style st = rp.getElement().getStyle();
+		final RootPanel	root = RootPanel.get();
+		Style st = root.getElement().getStyle();
 		st.setMargin(0.0, Unit.PX);
 		st.setPadding( 0.0, Unit.PX );
 		st.setBorderWidth(0.0, Unit.PX);
+		
+		final RootPanel	rp = RootPanel.get("cont");
+		/*Style st = rp.getElement().getStyle();
+		st.setMargin(0.0, Unit.PX);
+		st.setPadding( 0.0, Unit.PX );
+		st.setBorderWidth(0.0, Unit.PX);*/
 		Window.enableScrolling( false );
 		
-		int w = Window.getClientWidth();
+		/*int w = Window.getClientWidth();
 		int h = Window.getClientHeight();
 		rp.setSize(w+"px", (h-90)+"px");
 		
@@ -140,10 +189,32 @@ public class Compressor implements EntryPoint {
 				
 				rp.setSize(w+"px", (h-90)+"px");
 			}
+		});*/
+		
+		final FileUpload fu = new FileUpload();
+		fu.addChangeHandler( new ChangeHandler() {
+			@Override
+			public void onChange(ChangeEvent event) {
+				Browser.getWindow().getConsole().log("ermerm");
+				int fi = 0;
+				int nf = numberOfFiles( fu.getElement() );
+				Blob 				blob = readFile( fu.getElement(), fi );
+				while( blob != null ) {
+					String fn = getFilename( blob );
+					addFile( blob, fn, nf );
+					blob = readFile( fu.getElement(), ++fi );
+				}
+			}
 		});
 		
 		final VerticalPanel subvp = new VerticalPanel();
 		FocusPanel	fp = new FocusPanel();
+		/*st = fp.getElement().getStyle();
+		st.setMargin(0.0, Unit.PX);
+		st.setPadding( 0.0, Unit.PX );
+		st.setBorderWidth(0.0, Unit.PX);*/
+		
+		fp.getElement().getStyle().setBackgroundColor( "#EEEEEE" );
 		fp.addDropHandler( new DropHandler() {
 			@Override
 			public void onDrop(DropEvent event) {
@@ -155,7 +226,7 @@ public class Compressor implements EntryPoint {
 				int nf = numberOfFiles( event.getDataTransfer() );
 				Blob 				blob = readFile( event.getDataTransfer(), fi );
 				while( blob != null ) {
-					String fn = getFilename( blob );					
+					String fn = getFilename( blob );
 					addFile( blob, fn, nf );
 					blob = readFile( event.getDataTransfer(), ++fi );
 				}
@@ -185,10 +256,16 @@ public class Compressor implements EntryPoint {
 			@Override
 			public void onDrag(DragEvent event) {}
 		});
-		fp.setSize( "100%", "100%" );
+		fp.addDoubleClickHandler( new DoubleClickHandler() {
+			@Override
+			public void onDoubleClick(DoubleClickEvent event) {
+				click( fu.getElement() );
+			}
+		});
+		fp.setSize( "100%", "300px" );
 		
 		VerticalPanel vp = new VerticalPanel();
-		vp.setSize( "100%", "100%" );
+		vp.setSize( "100%", "300px" );
 		vp.setHorizontalAlignment( VerticalPanel.ALIGN_CENTER );
 		vp.setVerticalAlignment( VerticalPanel.ALIGN_MIDDLE );
 		
@@ -196,7 +273,9 @@ public class Compressor implements EntryPoint {
 		subvp.setVerticalAlignment( VerticalPanel.ALIGN_MIDDLE );
 		
 		subvp.add( new HTML("<h3>Compress files directly in your browser</h3>") );
-		subvp.add( new HTML("drag-drop (multiple) files to compress with zip") );
+		subvp.add( new HTML("drag-drop (multiple) files here to compress with zip") );
+		subvp.add( new HTML("double click to open a file dialog") );
+		subvp.add( fu );
 		vp.add( subvp );
 		
 		fp.add( vp );
